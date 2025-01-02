7ebe14c4258bea4de360d680b30200302a0d8e29
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-01-02 21:44:41 +0545

    Class

--- script.js
+++ script.js
@@ -1,32 +1,81 @@
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+
+// canvas.width = 600
+// canvas.height = 600
+
+// if (canvas && canvas.getContext('2d')) {
+//     var context = canvas.getContext('2d');
+//     var image = document.getElementById('dvdimage');
+//     context.drawImage(image, 35, 15);
+// }
+
+// let speed = 100;
+// let x = 0;
+// let lastTime = 0;
+
+// const gameLoop = (timestamp) => {
+
+//     const deltaTime = (timestamp - lastTime) / 1000;
+//     lastTime = timestamp;
+
+//     ctx.beginPath();
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     ctx.rect(x, 0, 30, 30);
+//     ctx.fill();
+//     x = x + speed * deltaTime;
+
+//     if (x >= canvas.width - 30) {
+//         speed = -speed;
+//         ctx.rect(x, 0, 30, 30);
+//         ctx.fill();
+//     };
+
+//     requestAnimationFrame(gameLoop);
+// };
+
+// requestAnimationFrame(gameLoop);
+
+
+
+
+// Jhinga fly
+
+import Spider from "./spider.js";
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let speed = 100;
-let x = 0;
-let y = 0;
-let lastTime = 0;
+canvas.style.border = '2px solid'
+
+canvas.width = 400;
+canvas.height = 400;
+
+const allSpiders = Array.from({ length: 100 }, () => {
+    const size = Math.random() * (30 - 10) + 10
+    return new Spider(
+        Math.random() * (canvas.width - 40),
+        Math.random() * (canvas.height - 40),
+        size,
+        size,
+    )
+})
+
+// Formula for min/max random
+// Math.random() * (max-min) + min;
+// Math.random() * (10-5) + 5;
 
 const gameLoop = (timestamp) => {
-    const deltaTime = (timestamp - lastTime) / 1000;
-    lastTime = timestamp
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.heght);
 
     ctx.beginPath();
-    ctx.rect(x, 0, 20, 20);
+    ctx.fillStyle = 'black';
+    ctx.rect(0, 0, 20, 20);
     ctx.fill();
-    x = x + speed * deltaTime;
 
-    if (x >= canvas.width - 20) {
-        speed = -speed;
-        x = canvas.width - 20;
-    } 
-    
-    const dx = document.getElementById('x');
-    const dy = document.getElementById('y');
+    allSpiders.forEach((spider) => spider.draw(ctx))
 
-    dx.innerHTML = x
-    dy.innerHTML = y
     requestAnimationFrame(gameLoop);
-}
+};
 
 requestAnimationFrame(gameLoop);
